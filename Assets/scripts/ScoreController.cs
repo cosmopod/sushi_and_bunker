711dfd305b6d5ac25ac4beb133dc228f441e89e4
cosmopod <fraurerod@gmail.com>
2014-08-02 23:00:26 +0200

    efectos en el marcador

--- Assets/scripts/ScoreController.cs
+++ Assets/scripts/ScoreController.cs
@@ -7,18 +7,19 @@ public class ScoreController : MonoBehaviour
 		protected int points = 0;
 		public static ScoreController Instance;
 		protected UILabel uiLabel;
-		
+		protected PlayerControllerGraph player;
 		
 		void Awake ()
 		{
 				Instance = this;
+				
 		}
 
 		// Use this for initialization
 		void Start ()
 		{
 				uiLabel = gameObject.GetComponent<UILabel> ();
-				
+				player = PlayerControllerGraph.Instance;
 		}
 	
 		// Update is called once per frame
@@ -29,13 +30,23 @@ public class ScoreController : MonoBehaviour
 
 		public void  UpdatePoints ()
 		{
-				//points = PlayerController.Instance.GetPoints ();
-				points = PlayerControllerGraph.Instance.GetPoints ();
+				if (points != player.GetPoints ()) {
+						points = player.GetPoints ();
+						StartCoroutine (TweenScore ());
+				}
+				
 				uiLabel.text = points.ToString () + " points";
 				
+				
+		}
+		IEnumerator TweenScore ()
+		{
+				uiLabel.color = Color.yellow;
+				TweenScale.Begin (gameObject, 0.2f, new Vector3 (1.5f, 1.5f, 1.5f));
+				yield return new WaitForSeconds (0.2f);
+				uiLabel.color = Color.white;
+				TweenScale.Begin (gameObject, 0.2f, new Vector3 (1.0f, 1.0f, 1.0f));
 		}
 		
-
-
-
+	
 }
