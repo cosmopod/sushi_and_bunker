0b72f0b0fce9966391a5aa0f52c8276d4bf7f020
cosmopod <fraurerod@gmail.com>
2014-08-03 16:30:51 +0200

    nuevo estado del personaje e implementacion de efectos sonoros

---
+++ Assets/scripts/EnvironmentController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+		protected PlayerControllerGraph player;
+		protected AudioSource audioSource;
+		protected float bottomLimitPitch = .65f;
+		protected float pitchChangeSpeed = .1f;
+		
+
+		// Use this for initialization
+		void Start ()
+		{
+				player = PlayerControllerGraph.Instance;
+				audioSource = gameObject.GetComponent<AudioSource> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				
+				if (player.isHungry) 
+						DownPitch ();
+				else if (!player.isHungry)
+						UpPitch ();
+			
+		}
+		
+		void DownPitch ()
+		{
+				if (audioSource.pitch >= bottomLimitPitch)
+						audioSource.pitch -= Time.deltaTime * pitchChangeSpeed;
+				
+			
+		}
+		void UpPitch ()
+		{
+				if (audioSource.pitch < 1.0f)
+						audioSource.pitch += Time.deltaTime;
+			
+		}
+		
+}
